835aefcede75d6730574937cd3068a54ccaa3532
Mariano Florencio Mendonca <mariano.mendonca@ish.com.br>
2024-03-18 22:42:14 -0300

    leitura de arquivo dados_transparencia.csv referente a informações do portal da transparencia, criação de modelo de recepção dos dados e metodo de leitura

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import os
 
+from domain.archive import Archive
 
 if __name__ == '__main__':
-    pass
+    file_name = "dados_transparencia.csv"
+    arquivo = Archive(file_name)
+    if os.path.exists(file_name):
+        print(arquivo.file_read())
+    else:
+        arquivo.create_archive()
