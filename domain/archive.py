f98e2eb7a6904703b76f56d25fc13070fed3017d
Mariano Florencio Mendonca <mariano.mendonca@ish.com.br>
2024-04-06 10:23:05 -0300

    Edição de arquivo e exclusão

--- domain/archive.py
+++ domain/archive.py
@@ -39,3 +39,57 @@ class Archive:
                 datas.append(transparence)
 
         return datas
+
+    def file_write(self, dados):
+        with open(self.path_file, "w", encoding="latin-1") as file:
+            gravador = csv.writer(file, delimiter=";")
+
+            # Escreve cabeçalho
+            gravador.writerow([
+                "CODIGO_ORGAO_SUPERIOR",
+                "NOME_ORGAO_SUPERIOR",
+                "CODIGO_ORGAO",
+                "NOME_ORGAO",
+                "CODIGO_UNIDADE_GESTORA",
+                "NOME_UNIDADE_GESTORA",
+                "CATEGORIA_ECONOMICA",
+                "ORIGEM_RECEITA",
+                "ESPECIE_RECEITA",
+                "DETALHAMENTO",
+                "VALOR_PREVISTO_ATUALIZADO",
+                "VALOR_LANCADO",
+                "VALOR_REALIZADO",
+                "PERCENTUAL_REALIZADO",
+                "DATA_LANCAMENTO",
+                "ANO_EXERCICIO",
+            ])
+
+            # Escreve os dados
+            for registro in dados:
+                gravador.writerow([
+                    registro.codigo_orgao_superior,
+                    registro.nome_orgao_superior,
+                    registro.codigo_orgao,
+                    registro.nome_orgao,
+                    registro.codigo_unidade_gestora,
+                    registro.nome_unidade_gestora,
+                    registro.categoria_economica,
+                    registro.origem_receita,
+                    registro.especie_receita,
+                    registro.detalhamento,
+                    registro.valor_previsto_atualizado,
+                    registro.valor_lancado,
+                    registro.valor_realizado,
+                    registro.percentual_realizado,
+                    registro.data_lancamento,
+                    registro.ano_exercicio,
+                ])
+
+    @staticmethod
+    def edit_file(dados, position, new_register):
+        dados[position] = new_register
+
+    @staticmethod
+    def delete_data(dados, position):
+
+        del dados[position]
--- main.py
+++ main.py
@@ -1,11 +1,61 @@
 import os
 
 from domain.archive import Archive
+from model.transparence import Transparence
 
 if __name__ == '__main__':
     file_name = "dados_transparencia.csv"
     arquivo = Archive(file_name)
+    data = []
     if os.path.exists(file_name):
-        print(arquivo.file_read())
+        data = arquivo.file_read()
+
+        # Edita o segundo registro
+        novo_registro = Transparence(
+            "123456",
+            "Novo nome órgão superior",
+            "1234",
+            "Novo nome órgão",
+            "5678",
+            "Nova nome unidade gestora",
+            "Nova categoria econômica",
+            "Nova origem receita",
+            "Nova espécie receita",
+            "Novo detalhamento",
+            "1000.00",
+            "900.00",
+            "800.00",
+            "80.00",
+            "2023-03-10",
+            "2024",
+        )
+        arquivo.edit_file(data, 1, novo_registro)
+
+        arquivo.delete_data(data, 2)
+
+        arquivo.file_write(data)
     else:
         arquivo.create_archive()
+
+        # Edita o segundo registro
+        novo_registro = Transparence(
+            "123456",
+            "Novo nome órgão superior",
+            "1234",
+            "Novo nome órgão",
+            "5678",
+            "Nova nome unidade gestora",
+            "Nova categoria econômica",
+            "Nova origem receita",
+            "Nova espécie receita",
+            "Novo detalhamento",
+            "1000.00",
+            "900.00",
+            "800.00",
+            "80.00",
+            "2023-03-10",
+            "2024",
+        )
+        arquivo.edit_file(data, 1, novo_registro)
+
+        arquivo.file_write(data)
